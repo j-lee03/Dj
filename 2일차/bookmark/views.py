fbfd327f49ef3275d2d9d5e77a78c9821ba9ea74
j.lee <sdf090400@gmail.com>
2025-11-14 14:14:50 +0900

    2

--- 2일차/bookmark/views.py
+++ 2일차/bookmark/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 def bookmark_list(request):
-    return HttpResponse('<h1>북마크리소스페이지</h1>')
+    return render(request, template_name='bookmark_list.html')
--- 2일차/config/urls.py
+++ 2일차/config/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-
+from bookmark import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookmark/', include('bookmark.urls')),
-
-
+    path('bookmark/',views.bookmark_list),
 
 
 ]
